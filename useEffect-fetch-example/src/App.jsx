46d2b195e2ab734c80ac6e413de15621648b03f9
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-04-01 14:37:29 +0200

    added code

--- useEffect-fetch-example/src/App.jsx
+++ useEffect-fetch-example/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState(null);
+  const [counter, setCounter] = useState(0);
   const url = 'https://jsonplaceholder.typicode.com/todos';
+
+  useEffect(() => {
+    
+  }, []);
+
+  const handleClick = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setTodos([...data]));
+  }
+
+  useEffect(() => {
+    
+    
+  }, [counter]);
 
   return (
     <div className="app">
-
+      <ul>
+        {
+          todos ? todos.map(todo => <li key={todo.id}>{todo.title}</li>) : <p>Loading...</p>
+        }
+      </ul>
+      <button onClick={handleClick}>ÖKA</button>
     </div>
   )
 }
