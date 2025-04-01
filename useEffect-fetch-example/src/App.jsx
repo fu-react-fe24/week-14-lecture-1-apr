fbfca3d2d040f22d9118412277e68c0b4004265b
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-04-01 13:03:04 +0200

    added start code

---
+++ useEffect-fetch-example/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+
+  return (
+    <div className="app">
+
+    </div>
+  )
+}
+
+export default App;
